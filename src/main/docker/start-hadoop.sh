4ac20495320037ea74618ff38c95663b6f023570
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-09-21 23:40:25 +0200

    solve docker container problems

--- src/main/docker/start-hadoop.sh
+++ src/main/docker/start-hadoop.sh
@@ -7,4 +7,6 @@ $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal ml-latest-small/ /
+
 tail -f /dev/null
