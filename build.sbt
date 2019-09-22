58cf3c17a608fb1d47885bdedd9fa46a16c9cd5a
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-09-22 16:01:53 +0200

    finally example works, now go on with it!

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.12.1"
 libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.4.4",
   "org.apache.spark" %% "spark-sql" % "2.4.4")
 
+libraryDependencies += "log4j" % "log4j" % "1.2.17"
+
--- src/main/scala/quickStart.scala
+++ src/main/scala/quickStart.scala
@@ -1,22 +1,25 @@
+/*
+launch notes:
+- add '127.0.0.1   localhost   hadoop' in etc/hosts
+- docker build -t container-name . (from docker/ folder)
+- docker run -ti --hostname hadoop -p 50070:50070 -p 9000:9000 -p 50075:50075 -p 50010:50010 container-name
+*/
 object QuickStartTest {
   case class Movie(movieId : Int ,title : String ,genres : Set[String])
   import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
-  import org.apache.hadoop.conf.Configuration
 
   def main ( args: Array[String] ): Unit = {
-    //TODO debit
-    Configuration.addDefaultResource("configs/core-site.xml")
-    Configuration.addDefaultResource("configs/hdfs-site.xml")
+    import org.apache.log4j.Logger
+    val log = Logger.getLogger(getClass.getName)
     val sc = SparkSession.builder().appName("Quick start")
       .master("local[*]")
       .config("spark.hadoop.dfs.client.use.datanode.hostname", "true")
       .getOrCreate().sqlContext
-    import org.apache.spark.sql.SQLImplicits
-    val ratings = loadDF(sc, "hdfs://hadoop:9000/ml-latest-small/ratings.csv")
-    ratings.show()
-   // val movies = sc.textFile("hdfs://localhost:9000/ml-latest-small/movies.csv")
-    //val count = movies.count()
-    //sqlContext.sparkSession.close()
+
+    val movies = loadDF(sc, "hdfs://hadoop:9000/ml-latest-small/movies.csv")
+    log.info(s"There are: ${movies.count()} movies")
+
+    sc.sparkSession.close()
   }
   def loadDF(sqlContext: SQLContext, filePath: String): DataFrame = sqlContext.read
     .format("com.databricks.spark.csv")
