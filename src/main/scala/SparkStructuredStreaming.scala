f16a8436dbb739fc4e3c6c7de191d4c275c21102
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-07 10:56:43 +0200

    refactor and explore functionalities

--- src/main/scala/SparkStructuredStreaming.scala
+++ src/main/scala/SparkStructuredStreaming.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.{DataFrame, Dataset}
 
 object SparkStructuredStreaming {
 
@@ -6,36 +7,48 @@ object SparkStructuredStreaming {
   import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
   import org.apache.spark.sql.functions._
 
+  type StreamType = String
+
   def main ( args: Array[String] ): Unit = {
     //val log = org.apache.log4j.Logger.getLogger ( getClass.getName )
 
-    val spark = SparkSession.builder ().appName ( "Quick start" )
+    val spark = SparkSession.builder.appName ( "Structured Streaming" )
       .master ( "local[*]" )
       .getOrCreate
     spark.sparkContext.setLogLevel ( "ERROR" )
 
-    /*val socketDF = spark.readStream
-      .format ( "socket" )
-      .options ( Map ( "host" -> "localhost", "port" -> "9999" ) )
-      .load
-      */
-    val socketDF1 = spark.readStream
-      .format ( "rate" ) //produce data for testing
-      .option ( "rowsPerSecond", 10 )
-      .load
-    val socketDF2 = spark.readStream.format ( "rate" ).option ( "rowsPerSecond", 10 ).load
+    def getRateDataFrame: DataFrame = {
+      spark.readStream
+        .format ( "rate" ) //produce data for testing
+        .option ( "rowsPerSecond", 10 )
+        .load
+    }
 
-    socketDF1.isStreaming
-    socketDF1.printSchema
+    /*val socketDF = spark.readStream
+          .format ( "socket" )
+          .options ( Map ( "host" -> "localhost", "port" -> "9999" ) )
+          .load
+          */
+    val dataFrame1 = getRateDataFrame
+    val dataFrame2 = getRateDataFrame
+
+    dataFrame1.isStreaming
+    dataFrame1.printSchema
     import spark.implicits._
 
-    val socketDS1 = socketDF1.as[(String, Timestamp)]
-      .withColumn ( "length", length ( 'value ) )
-      //define your scala function! with udf (but there is a problem.. serialization)
-      .as [StreamRecord] //use case class and typing of SparkSQL
+    def getDataset ( dataFrame: DataFrame ): Dataset[StreamRecord] = {
+      dataFrame.as[(StreamType, Timestamp)]
+        .withColumn ( "length", length ( 'value ) )
+        // or .withColumn ( "length", udf { s: StreamType => s.toString.length }.apply ( col("value") ) )
+        //define your scala function with udf (but there is a problem.. serialization)
+        .as [StreamRecord] //use case class and typing of SparkSQL
+    }
+
+    val dataset1 = getDataset ( dataFrame1 )
+    val dataset2 = getDataset ( dataFrame2 )
 
     //group by key and count
-    socketDS1
+    dataset1
       .withWatermark ( "timestamp", "10 seconds" )
       .groupBy (
         window ( $"timestamp", "10 seconds", "5 seconds" ),
@@ -49,18 +62,14 @@ object SparkStructuredStreaming {
       .start
 
     //udf function
-    socketDS1.select ( udf { s: String => s.hashCode }.apply ( 'value ).as ( "myUDF" ) ).writeStream.format ( "console" ).start
+    dataset1.select ( udf { s: StreamType => s.hashCode }.apply ( 'value ).as ( "hash code" ) ).writeStream.format ( "console" ).start
 
     //join 2 Dataset
-    val socketDS2 = socketDF2.as[(String, Timestamp)]
-      .withColumn ( "length", udf { s: String => s.length }.apply ( 'value ) )
-      .as [StreamRecord]
-    socketDS1.join ( socketDF2, "value" ).select ( "*" ).writeStream.format ( "console" ).start
+    dataset1.join ( dataset2, "value" ).select ( "*" ).writeStream.format ( "console" ).start
 
     //await any computation
     spark.streams.awaitAnyTermination
   }
 
-  case class StreamRecord ( value: String, timestamp: Timestamp, length: Int )
-
+  case class StreamRecord ( value: StreamType, timestamp: Timestamp, length: Int )
 }
