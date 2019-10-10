4ca462cf1c12b845d6e562cb8107c13ce778062f
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-10 15:13:19 +0200

    first working version with windows, aggregation of columns values

--- src/main/scala/structuredStreaming/SparkStructuredStreaming.scala
+++ src/main/scala/structuredStreaming/SparkStructuredStreaming.scala
@@ -3,17 +3,16 @@ package structuredStreaming
 object SparkStructuredStreaming {
   def main ( args: Array[String] ): Unit = {
     import StreamingDataOrganizerUtils._
-    import StreamingDataOrganizerUtils.structuredManipulationUtilities._
     import StreamingDataOrganizerUtils.structuredManipulationUtilities.ColumnOperations._
     import org.apache.spark.sql.functions._
     import org.apache.spark.sql.streaming.Trigger
 
-    val dataset1 = getDataset ( 10 )
-    val dataset2 = getDataset ( 20 )
-    val dataset3 = getDataset ( 30 )
+    val dataset1 = getDataFrame ( 10 )
+    val dataset2 = getDataFrame ( 20 )
+    val dataset3 = getDataFrame ( 30 )
 
     //udf function
-    dataset1.select ( udf { s: Long => s.hashCode }.apply( idColumn ).as ( "hash code" ) )
+    dataset1.select ( udf { s: Long => s.hashCode }.apply ( idColumn ).as ( "hash code" ) )
       .writeStream
       .trigger ( Trigger.ProcessingTime ( "2 seconds" ) )
       //.trigger ( Trigger.Continuous ( "5 seconds" ) ) //use experimental CP (in Spark 2.3+).. don't work :)
@@ -24,23 +23,19 @@ object SparkStructuredStreaming {
     val datasets = dataset1.join ( dataset2.toDF, idColumn.name )
       .join ( dataset3, idColumn.name )
       .withColumn ( avgValueColumn.name,
-      avgOfColumns (dataset1.col ( valueColumn.name ), dataset2.col ( valueColumn.name ), dataset3.col ( valueColumn.name ) ) )
-  /*.withColumn ( maxLengthColumn.name,
-       maxOfColumns ( dataset1.col ( lengthColumn.name )+ dataset2.col ( lengthColumn.name ) + dataset3.col ( lengthColumn.name )  ))*/
-    datasets//.drop ( datasets.columns.filter ( _.equals ( valueColumn.name ) ): _* )
-      //.drop ( datasets.columns.filter ( _.equals ( lengthColumn.name ) ): _* )
-      //.withColumnRenamed(avgValueColumn.value, valueColumn.value)
-      /* .withWatermark ( timestampColumn.value, "5 seconds")
-       .groupBy (
-         window ( timestampColumn.column, "10 seconds", "5 seconds" ),
-         dataset1.col(lengthColumn.value)) //TODO don't work
-       .count*/
-      .select ( "*" )
+        avgOfColumns ( dataset1.col ( valueColumn.name ), dataset2.col ( valueColumn.name ), dataset3.col ( valueColumn.name ) ) )
+      .withColumn ( avgTimestampColumn.name,
+        to_timestamp(avgOfColumns ( unix_timestamp(dataset1.col ( timestampColumn.name )), unix_timestamp(dataset2.col ( timestampColumn.name )), unix_timestamp(dataset3.col ( timestampColumn.name ) ))) )
+
+    datasets.drop ( datasets.columns.filter ( _.equals ( valueColumn.name ) ): _* )
+      .drop ( datasets.columns.filter ( _.equals ( timestampColumn.name ) ): _* )
+      .withWatermark ( avgTimestampColumn.name, "5 seconds")
+      .groupBy (window ( avgTimestampColumn, "10 seconds", "5 seconds" ))
+      .mean(avgValueColumn.name)
       .writeStream
       .format ( "console" )
       .start
 
-    //await any computation
     spark.streams.awaitAnyTermination
   }
 
--- src/main/scala/structuredStreaming/StreamingDataOrganizerUtils.scala
+++ src/main/scala/structuredStreaming/StreamingDataOrganizerUtils.scala
@@ -18,31 +18,28 @@ object StreamingDataOrganizerUtils {
   val timestampColumn: DatasetColumn = DatasetColumn ( "timestamp" )
   val valueColumn: DatasetColumn = DatasetColumn ( "value" )
   val avgValueColumn: DatasetColumn = DatasetColumn ( "avgValue" )
-  val maxLengthColumn: DatasetColumn = DatasetColumn ( "maxLength" )
+  val avgTimestampColumn: DatasetColumn = DatasetColumn ( "avgTimestamp" )
 
-  def getDataset ( rowsPerSecond: Int): Dataset[StreamRecord] = {
+  def getDataFrame ( rowsPerSecond: Int ): Dataset[StreamRecord] = {
     //produce data for testing (can also define the schema)
     spark.readStream.format ( "rate" ).option ( "rowsPerSecond", rowsPerSecond ).load
-      .withColumnRenamed("value", idColumn.name)
-      .withColumn ( valueColumn.name, rand * 100)
+      .withColumnRenamed ( "value", idColumn.name )
+      .withColumn ( valueColumn.name, rand * 100 )
       // or .withColumn ( valueColumn.name, udf { s: StreamType => ??? }.apply ( 'value ) )
       //define your scala function with udf (but there is a problem.. serialization)
-      .as [StreamRecord] //use case class and typing of SparkSQL, there's the difference between a dataset and a dataFrame
+      .as [StreamRecord] //use case class and typing of SparkSQL, there's the difference between a Dataset and a dataFrame
   }
 
   object structuredManipulationUtilities {
 
     import org.apache.spark.sql.Column
 
-    case class DatasetColumn ( name: String ) extends Column ( Symbol(name).expr )
+    case class DatasetColumn ( name: String ) extends Column ( Symbol ( name ).expr )
 
     case class StreamRecord ( id: Long, timestamp: Timestamp, value: Double )
 
     object ColumnOperations {
-      // TODO pass type
-      def avgOfColumns ( columns: Column* ): Column = columns.reduce ( _.as [Double] + _ ) / columns.size
-
-      def maxOfColumns ( columns: Column* ): Column = columns ( 0 ) // TODO map(_.as[T]).max
+      def avgOfColumns ( columns: Column* ): Column = columns.reduce ( _ + _ ) / columns.size
     }
 
   }
