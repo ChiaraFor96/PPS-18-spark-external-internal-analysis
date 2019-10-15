a4cf2c07286f4787d257190f41dce1c10af84c09
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-16 00:21:26 +0200

    try stream container as hdfs, instead of netcat beacuse there were problems in keep single command connection in while true

--- src/main/docker/stream/writestream.sh
+++ src/main/docker/stream/writestream.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-#TODO PORT, HOST=`hostname --fqdn`
+
+/etc/init.d/ssh start
+
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
 i=0
-while true; do
+while :
+do
   ((i+=1))
-  echo -e "date: $(date);value: $i \r\n" | nc -l 9999
-  sleep 0.1
+  echo "$(date), $i" >> data.csv
+  $HADOOP_HOME/bin/hdfs dfs -copyFromLocal -f data.csv /
+  sleep 10
 done
 
 tail -f /dev/null
