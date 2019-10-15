b85f69197140548a121f8775b78dbe99c3f73036
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-16 00:50:55 +0200

    try to solve with append to file..but seems change nothing

--- src/main/docker/stream/writestream.sh
+++ src/main/docker/stream/writestream.sh
@@ -9,11 +9,11 @@ $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 
 i=0
 echo "date, value" >> data.csv
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal -f data.csv /
 while :
 do
   ((i+=1))
-  echo "$(date), $i" >> data.csv
-  $HADOOP_HOME/bin/hdfs dfs -copyFromLocal -f data.csv /
+  echo "$(date), $i" | $HADOOP_HOME/bin/hdfs dfs -appendToFile - data.csv
   sleep 10
 done
 
