3e9f96e0d19baa7a465bd60eb802955e3159d322
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-15 18:47:22 +0200

    something seems work

--- src/main/docker/stream/writestream.sh
+++ src/main/docker/stream/writestream.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #TODO PORT, HOST=`hostname --fqdn`
+i=0
 while true; do
-  echo -e "date: $(date);value: 100 \r\n" | nc -l -p 9999
+  ((i+=1))
+  echo -e "date: $(date);value: $i \r\n" | nc -l 9999
   sleep 0.1
 done
 
 tail -f /dev/null
-#TODO $(jot -r 1 0 100)
