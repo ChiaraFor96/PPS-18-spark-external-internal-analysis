4ebb8459f7db7dc52a601707011ee7f7f74b3a2f
Chiara Forresi <chiara.forresi@studio.unibo.it>
2019-10-19 19:50:44 +0200

    go on with testing in basic

--- src/main/scala/QuickStartBasic.scala
+++ src/main/scala/QuickStartBasic.scala
@@ -5,58 +5,96 @@ launch notes:
 - docker run -ti --hostname hadoop -p 50070:50070 -p 9000:9000 -p 50075:50075 -p 50010:50010 container-name
 */
 object QuickStartBasic {
+
   import org.apache.spark.rdd.RDD
   import org.apache.spark.{SparkConf, SparkContext}
 
   def main ( args: Array[String] ): Unit = {
-    val conf = new SparkConf().setAppName("Quick start basic")
-      .setMaster("local[*]")
-      .set("spark.hadoop.dfs.client.use.datanode.hostname", "true" )
-    val sc = SparkContext.getOrCreate(conf)
+    val conf = new SparkConf ().setAppName ( "Quick start basic" )
+      .setMaster ( "local[*]" )
+      .set ( "spark.hadoop.dfs.client.use.datanode.hostname", "true" )
+    val sc = SparkContext.getOrCreate ( conf )
 
-    sc.setLogLevel("ERROR")
+    sc.setLogLevel ( "ERROR" )
 
-    val movies = loadRDD ( sc, "hdfs://hadoop:9000/ml-latest-small/movies.csv" )
-    val moviesHeader = new SimpleCSVHeader(movies.first)
-    val moviesRows = movies.filter(moviesHeader(_,"movieId") != "movieId") // filter the header out
+    val (moviesHeader, moviesRows) = loadRDD ( sc, MoviesTest.moviesSource )
 
-    println(s"Schema: ${movies.first.mkString(" ")}")
-    println(s"There are: ${moviesRows.count} movies")
-    println(moviesRows.take(5).map(_.mkString(" ")).mkString("\n"))
+    countMovies ( moviesRows )
+    checkSchema ( moviesHeader )
+    checkFirstFiveRows ( moviesRows )
+    movieWithLongestTitle ( moviesHeader, moviesRows )
+    moviesWithYear ( moviesHeader, moviesRows )
+    topThreeGenres ( moviesHeader, moviesRows )
+    threeMoviesWithMostTags ( sc, moviesHeader, moviesRows )
+    // Thread.sleep ( 20000000 ) for see on the spark UI
+    sc.stop
+  }
 
-    println(s"Movie with longer title ${moviesRows.map(moviesHeader(_, "title"))
-      .map(t => (t, t.length))
-      .sortBy(_._2, ascending = false)
-      .take(1).mkString(" ")}")
-
-    println(s"#Movies with year: ${moviesRows.map(moviesHeader(_, "title"))
-        .filter("""[0-9]{4}""".r.findFirstIn(_).isDefined)
-        .count
-    }")
-
+  private def topThreeGenres ( moviesHeader: SimpleCSVHeader, moviesRows: RDD[Array[String]] ) {
     /* TODO
     movies.select ( split ( movies.col ( "genres" ), "\\|" ).as ( "genres" ) )
       .select ( explode ( new Column ( "genres" ) ).as ( "genre" ) ).groupBy ( "genre" )
       .count.show ( 3 )*/
-
-    //load tags
-    val tags = loadRDD ( sc, "hdfs://hadoop:9000/ml-latest-small/tags.csv" )
-    val tagsHeader = new SimpleCSVHeader(tags.first)
-    val tagsRows = tags.filter(tagsHeader(_,"movieId") != "movieId") // filter the header out
-
-    /* TODO
-    tags
-      .groupBy ( "movieId" ).count.sort ( desc ( "count" ) ).limit ( 3 )
-      .join ( movies, "movieId" ).select ( "title", "count" ).show ()*/
-
-    // Thread.sleep ( 20000000 ) for see on the spark UI*/
-    sc.stop
   }
 
-  def loadRDD ( sc: SparkContext, filePath: String ): RDD[Array[String]] = sc.textFile(filePath).map(_.split(",").map(_.trim))
+  private def threeMoviesWithMostTags ( sc: SparkContext, moviesHeader: SimpleCSVHeader, moviesRows: RDD[Array[String]] ): Unit = {
+    val (tagsHeader, tagsRows) = loadRDD ( sc, MoviesTest.tagsSource )
 
-  class SimpleCSVHeader(header:Array[String]) extends Serializable {
+    val threeMoviesWithMostTags = sc.parallelize ( tagsRows.map ( tag => (tagsHeader ( tag, MoviesTest.movieId ), 1) )
+      .reduceByKey ( _ + _ )
+      .sortBy ( _._2, ascending = false )
+      .take ( 3 ) )
+      .join ( moviesRows.map ( movie =>
+        (moviesHeader ( movie, MoviesTest.movieId ), moviesHeader ( movie, MoviesTest.title )) ) )
+      .map { case (_, (x, y)) => (y, x) }.collect.toSet
+    assert ( threeMoviesWithMostTags == MoviesTest.threeMoviesWithMostTags )
+    println ( threeMoviesWithMostTags )
+  }
+
+  private def moviesWithYear ( moviesHeader: SimpleCSVHeader, moviesRows: RDD[Array[String]] ): Unit = {
+    val moviesWithYears = moviesRows.map ( moviesHeader ( _, MoviesTest.title ) )
+      .filter ( _.matches ( s"(.*)${MoviesTest.yearRegex}(.*)" ) )
+      .count
+    //TODO assert(moviesWithYears == MoviesTest.moviesWithYears)
+    println ( s"#Movies with year: $moviesWithYears" )
+  }
+
+  private def movieWithLongestTitle ( moviesHeader: SimpleCSVHeader, moviesRows: RDD[Array[String]] ): Unit = {
+    val movieWithLongerTitle = moviesRows.map ( moviesHeader ( _, MoviesTest.title ) )
+      .map ( t => (t, t.length) )
+      .sortBy ( _._2, ascending = false )
+      .take ( 1 ).head
+    assert ( movieWithLongerTitle == MoviesTest.movieWithLongerTitle )
+    println ( s"Movie with longer title $movieWithLongerTitle" )
+  }
+
+  private def checkFirstFiveRows ( moviesRows: RDD[Array[String]] ): Unit = {
+    val firstFiveMovies = moviesRows.take ( 5 ).map ( _.mkString ( " " ) )
+    assert ( firstFiveMovies sameElements MoviesTest.firstFiveMovies )
+    println ( firstFiveMovies.mkString ( "\n" ) )
+  }
+
+  private def checkSchema ( moviesHeader: SimpleCSVHeader ): Unit = {
+    assert ( moviesHeader.header.toSet == MoviesTest.movieSchema )
+    println ( s"Schema: ${moviesHeader.header.mkString ( " " )}" )
+  }
+
+  private def countMovies ( moviesRows: RDD[Array[String]] ): Unit = {
+    assert ( moviesRows.count == MoviesTest.numberOfMovies )
+    println ( s"There are: ${moviesRows.count} movies" )
+  }
+
+  private def loadRDD ( sc: SparkContext, filePath: String ): (SimpleCSVHeader, RDD[Array[String]]) = {
+    val rdd = sc.textFile ( filePath ).map ( _.split ( "," ).map ( _.trim ) )
+    val header = new SimpleCSVHeader ( rdd.first )
+    val rows = rdd.filter ( header ( _, MoviesTest.movieId ) != MoviesTest.movieId )
+    (header, rows)
+  }
+
+  private class SimpleCSVHeader ( val header: Array[String] ) extends Serializable {
     val index: Map[String, Int] = header.zipWithIndex.toMap
-    def apply(array:Array[String], key:String):String = array(index(key))
+
+    def apply ( array: Array[String], key: String ): String = array ( index ( key ) )
   }
+
 }
